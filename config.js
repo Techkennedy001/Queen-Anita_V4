66fb8bd8182e82773fb847fc5687f420436c61ef
Techkennedy001 <kennedywanyoike3747@gmail.com>
2025-02-21 13:06:20 +0300

    Update config.js

--- config.js
+++ config.js
@@ -2,13 +2,13 @@ require('dotenv').config();
 
 global.owner = process.env.OWNER_NUMBER.split(','); 
 global.anitav4 = process.env.BOT_NAME || "𝐐𝐔𝐄𝐄𝐍_𝐀𝐍𝐈𝐓𝐀-𝐕𝟒";
-global.ownername = process.env.OWNER_NAME || "David Cyril";
+global.ownername = process.env.OWNER_NAME || "Jabalken";
 global.packname = process.env.PACK_NAME || "𝐐𝐔𝐄𝐄𝐍_𝐀𝐍𝐈𝐓𝐀-𝐕𝟒";
 global.author = process.env.AUTHOR || "𝐃𝐀𝐕𝐈𝐃 𝐂𝐘𝐑𝐈𝐋";
 global.channelname = process.env.CHANNEL_NAME || "𝐃𝐀𝐕𝐈𝐃 𝐂𝐘𝐑𝐈𝐋";
 global.channeljid = process.env.CHANNEL_JID || "120363315231436175@newsletter";
-global.autoTyping = process.env.AUTO_TYPING === 'true'; 
-global.autoRecord = process.env.AUTO_RECORD === 'true';
+global.autoTyping = process.env.AUTO_TYPING === 'false'; 
+global.autoRecord = process.env.AUTO_RECORD === 'false';
 global.autoViewStatus = process.env.AUTO_VIEW_STATUS === 'true';
 global.AUTOSTATUS_REACT = process.env.AUTOSTATUS_REACT === 'true';
 global.autoLikeEmoji = process.env.AUTO_LIKE_EMOJI || "💚";
@@ -35,15 +35,15 @@ global.public = process.env.PUBLIC === 'true';
 global.ANTIDELETE = process.env.ANTIDELETE === 'true';
 global.unavailable = process.env.UNAVAILABLE === 'true';
 global.available = process.env.AVAILABLE === 'true';
-global.autoreadmessages = process.env.AUTO_READ_MESSAGES === 'true';
+global.autoreadmessages = process.env.AUTO_READ_MESSAGES === 'false';
 global.chatbot = process.env.CHATBOT === 'true';
 global.autoreact = process.env.AUTO_REACT === 'true';
 
-global.WELCOME = process.env.WELCOME === 'true';
+global.WELCOME = process.env.WELCOME === 'false';
 
 global.prefix = process.env.PREFIX || '.';
 
-global.autobio = process.env.AUTO_BIO === 'true';
+global.autobio = process.env.AUTO_BIO === 'false';
 
 global.ANTICALL = process.env.ANTICALL === 'true';
 
